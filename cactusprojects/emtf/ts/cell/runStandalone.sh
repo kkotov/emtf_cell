369cadfd9ba0bb60da9b7bf5e014b96c8107c9cf
emtfts <none@unknown.org>
2016-08-09 15:17:08 +0200

    Small changes in the toplevel xml file and the run standalone script.

--- cactusprojects/emtf/ts/cell/runStandalone.sh
+++ cactusprojects/emtf/ts/cell/runStandalone.sh
@@ -2,10 +2,10 @@
 
 HERE=$(python -c "import os.path; print os.path.dirname(os.path.abspath('$BASH_SOURCE'))")
 export EMTF_ROOT=${HERE}
-export LD_LIBRARY_PATH="${EMTF_ROOT}/lib/linux/x86_64_slc6/:${EMTF_ROOT}/../../pciExprLinuxBusAdapter/lib/linux/x86_64_slc6/"
+export LD_LIBRARY_PATH="/home/emtfts/cactus_trunk/cactusprojects/emtf/pciExprLinuxBusAdapter/lib/linux/x86_64_slc6/:${EMTF_ROOT}/lib/linux/x86_64_slc6/"
 
 export SUBSYSTEM_ID="$USER"
-SUBSYSTEM_CELL_LIB_PATH="file:///${EMTF_ROOT}/../../pciExprLinuxBusAdapter/lib/linux/x86_64_slc6/libPCIExprLinuxBusAdapter.so"
+SUBSYSTEM_CELL_LIB_PATH="file:///home/emtfts/cactus_trunk/cactusprojects/emtf/pciExprLinuxBusAdapter/lib/linux/x86_64_slc6/libPCIExprLinuxBusAdapter.so"
 SUBSYSTEM_CELL_LIB_PATH="$SUBSYSTEM_CELL_LIB_PATH;file:///${EMTF_ROOT}/lib/linux/x86_64_slc6/libemtfcell.so;"
 
 export SUBSYSTEM_CELL_LIB_PATH
@@ -13,8 +13,7 @@ export SUBSYSTEM_CELL_CLASS='emtf::Cell'
 
 export SWATCH_DEFAULT_INIT_FILE='${EMTF_ROOT}/exampleP5.xml'
 export SWATCH_DEFAULT_GATEKEEPER_XML='${EMTF_ROOT}/emtfTopLevel.xml'
-export SWATCH_DEFAULT_GATEKEEPER_KEY='MWGR4'
+export SWATCH_DEFAULT_GATEKEEPER_KEY='emtf_key'
 
-# /opt/xdaq/bin/swatchcell/runSubsystemCell.sh
 ./runSubsystemCell.sh
 
