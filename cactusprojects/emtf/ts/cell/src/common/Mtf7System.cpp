7a87a9edc6cdf93a62aeca83f2546cfeb08f6656
kkotov <kkotov@cern.ch>
2016-09-26 19:36:14 +0200

    Metric in one word

--- cactusprojects/emtf/ts/cell/src/common/Mtf7System.cpp
+++ cactusprojects/emtf/ts/cell/src/common/Mtf7System.cpp
@@ -71,7 +71,7 @@ Mtf7System::Mtf7System(const swatch::core::AbstractStub& aStub) :
     vector<AbstractMetric*> brokenLinksMetrics;
     for(auto it=getProcessors().begin(); it!=getProcessors().end(); ++it)
     {
-        brokenLinksMetrics.push_back(&(*it)->getMetric("Number of broken input links"));
+        brokenLinksMetrics.push_back(&(*it)->getMetric("numberOfBrokenInputLinks"));
     }
 
     ComplexMetric<uint16_t> & brokenLinks = registerComplexMetric<uint16_t>("Total number of broken input links", brokenLinksMetrics.begin(), brokenLinksMetrics.end(), ComplexMetric<uint16_t>::CalculateFunction_t(&countBrokenLinks));
