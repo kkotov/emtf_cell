422aeacf5ca240a395d944d00c8dffeb729310ca
emtfts <none@unknown.org>
2016-10-20 17:49:53 +0200

    A marker is added to the input links log file when e run starts.

--- cactusprojects/emtf/ts/cell/src/common/TransitionCommands.cpp
+++ cactusprojects/emtf/ts/cell/src/common/TransitionCommands.cpp
@@ -3,11 +3,21 @@
 #include "emtf/ts/cell/Mtf7Port.hpp"
 #include "emtf/ts/cell/TransitionCommands.hpp"
 #include <vector>
+#include "emtf/ts/cell/Mtf7Common.hpp"
+#include <boost/thread/barrier.hpp>
+#include <log4cplus/logger.h>
+#include <log4cplus/loggingmacros.h>
 
 namespace emtf {
 
 using namespace swatch::core;
 using namespace swatch::processor;
+using namespace log4cplus;
+
+
+// The whole boost::barrier will be removed once we migrate to the monitoring database and stopped using log files for the status of the input links.
+// The number of processors the EMTF has is 12. This is very unlikely to change for the future and this is why the value is hard-coded.
+static boost::barrier runStartBarrier(12);
 
 
 OnStart::OnStart(const std::string& aId, swatch::core::ActionableObject& aActionable) :
@@ -17,6 +27,12 @@ OnStart::OnStart(const std::string& aId, swatch::core::ActionableObject& aAction
 
 Command::State OnStart::code(const swatch::core::XParameterSet& params)
 {
+    if(runStartBarrier.wait())
+    {
+        log4cplus::Logger linkLogger(Logger::getInstance(config::log4cplusLinkLogger()));
+        LOG4CPLUS_TRACE(linkLogger, LOG4CPLUS_TEXT("###   Beginning of run XXX XXX   ###"));
+    }
+
     setStatusMsg("Executed at the transition from 'Aligned' to 'Running'");
     Mtf7Processor &processor = getActionable<Mtf7Processor>();
 
