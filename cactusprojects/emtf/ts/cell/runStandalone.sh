dbc17b2063a0ddb1eef6b780f3dbcf8f4d64396f
batinkov <atanas.batinkov@cern.ch>
2017-02-24 12:51:56 +0100

    Some paths are fixed

--- cactusprojects/emtf/ts/cell/runStandalone.sh
+++ cactusprojects/emtf/ts/cell/runStandalone.sh
@@ -2,12 +2,11 @@
 
 HERE=$(python -c "import os.path; print os.path.dirname(os.path.abspath('$BASH_SOURCE'))")
 export EMTF_ROOT=${HERE}
-export LD_LIBRARY_PATH="/home/emtfts/cactus_trunk/cactusprojects/emtf/pciExprLinuxBusAdapter/lib/linux/x86_64_slc6/:${EMTF_ROOT}/lib/linux/x86_64_slc6/"
+export LD_LIBRARY_PATH="${EMTF_ROOT}../../pciExprLinuxBusAdapter/lib/linux/x86_64_centos7/:${EMTF_ROOT}/lib/linux/x86_64_centos7/"
 
 export SUBSYSTEM_ID=emtf
-#"$USER"
-SUBSYSTEM_CELL_LIB_PATH="file:///home/emtfts/cactus_trunk/cactusprojects/emtf/pciExprLinuxBusAdapter/lib/linux/x86_64_slc6/libPCIExprLinuxBusAdapter.so"
-SUBSYSTEM_CELL_LIB_PATH="$SUBSYSTEM_CELL_LIB_PATH;file:///${EMTF_ROOT}/lib/linux/x86_64_slc6/libemtfcell.so;"
+SUBSYSTEM_CELL_LIB_PATH="$SUBSYSTEM_CELL_LIB_PATH;file:///${EMTF_ROOT}/../../pciExprLinuxBusAdapter/lib/linux/x86_64_centos7/libPCIExprLinuxBusAdapter.so"
+SUBSYSTEM_CELL_LIB_PATH="$SUBSYSTEM_CELL_LIB_PATH;file:///${EMTF_ROOT}/lib/linux/x86_64_centos7/libemtfcell.so;"
 
 export SUBSYSTEM_CELL_LIB_PATH
 export SUBSYSTEM_CELL_CLASS='emtf::Cell'
