917c82809cad75939163bd10b37faa5f112fe2d4
batinkov <atanas.batinkov@cern.ch>
2017-03-09 18:49:53 +0100

    A skeleton for alignment fifo delays is added.

--- cactusprojects/emtf/ts/cell/include/emtf/ts/cell/AlignmentFifoDelayCommands.hpp
+++ cactusprojects/emtf/ts/cell/include/emtf/ts/cell/AlignmentFifoDelayCommands.hpp
@@ -5,13 +5,16 @@
 
 namespace emtf {
 
-// class CheckFWVersion : public swatch::core::Command
-// {
-// public:
-//     CheckFWVersion(const std::string& aId, swatch::core::ActionableObject& aActionable);
-// 
-//     virtual swatch::core::Command::State code(const swatch::core::XParameterSet& params);
-// };
+class AlignmentFifoDelay : public swatch::core::Command
+{
+public:
+    AlignmentFifoDelay(const std::string& aId, swatch::core::ActionableObject& aActionable);
+
+    virtual swatch::core::Command::State code(const swatch::core::XParameterSet& params);
+
+private:
+    bool setFixedAlignmentFifoDelays(int endcap, int sector);
+};
 
 } // namespace
 
--- cactusprojects/emtf/ts/cell/src/common/AlignmentFifoDelayCommands.cpp
+++ cactusprojects/emtf/ts/cell/src/common/AlignmentFifoDelayCommands.cpp
@@ -1,8 +1,7 @@
-#include "xdata/String.h"
-#include "xdata/Integer.h"
+#include "emtf/ts/cell/AlignmentFifoDelayCommands.hpp"
 #include "emtf/ts/cell/Mtf7Processor.hpp"
-#include "emtf/ts/cell/Mtf7Common.hpp"
 #include "emtf/ts/cell/Mtf7ConfigCommands.hpp"
+#include "xdata/Boolean.h"
 
 using namespace std;
 using namespace swatch;
@@ -11,36 +10,44 @@ using namespace core;
 
 namespace emtf {
 
-// CheckFWVersion::CheckFWVersion(const std::string& aId, swatch::core::ActionableObject& aActionable) :
-//     swatch::core::Command(aId, aActionable, xdata::Integer(0))
-// {
-//     registerParameter("control_firmware_version", xdata::String("yyyy-mm-dd hh:mm:ss"));
-//     registerParameter("core_firmware_version", xdata::String("YYYY-MM-DD HH:MM:SS"));
-// }
+AlignmentFifoDelay::AlignmentFifoDelay(const std::string& aId, swatch::core::ActionableObject& aActionable) :
+    swatch::core::Command(aId, aActionable, xdata::Integer(0))
+{
+    registerParameter("enable_auto_delays", xdata::Boolean(true));
+}
 
-// swatch::core::Command::State CheckFWVersion::code(const swatch::core::XParameterSet& params)
-// {
-//     setStatusMsg("Compare the firmware version of the board with the one from the DB.");
-//     Mtf7Processor &processor = getActionable<Mtf7Processor>();
-// 
-//     const xdata::String controlFwVersion(params.get<xdata::String>("control_firmware_version").value_);
-//     const xdata::String coreFwVersion(params.get<xdata::String>("core_firmware_version").value_);
-// 
-//     const string controlFwVersionBoard = processor.readControlFirmwareVersion();
-//     const string coreFwVersionBoard = processor.readCoreFirmwareVersion();
-// 
-//     Command::State commandStatus = ActionSnapshot::kDone;
-// 
-//     if(   (controlFwVersion.toString() != controlFwVersionBoard)
-//        || (coreFwVersion.toString() != coreFwVersionBoard))
-//     {
-//         commandStatus = ActionSnapshot::kError;
-//     }
-// 
-//     setProgress(1.);
-// 
-//     return commandStatus;
-// }
+swatch::core::Command::State AlignmentFifoDelay::code(const swatch::core::XParameterSet& params)
+{
+    setStatusMsg("Enable the automatic alignment of the fifo delays");
+    Mtf7Processor &processor = getActionable<Mtf7Processor>();
+
+    // TODO: test if the auto_delays is set correctly
+    const xdata::Boolean auto_delays(params.get<xdata::Boolean>("enable_auto_af_delays").value_);
+    cout << "THE VALUE OF AUTO_DELAYS IS: " << auto_delays << endl;
+
+    processor.write64("af_en", auto_delays); // enable or disable the auto af delays
+
+    Command::State commandStatus = ActionSnapshot::kDone;
+
+    if(!auto_delays) // in case of auto delays disabled set fixed values for alignment filo delays
+    {
+        bool res = setFixedAlignmentFifoDelays(processor.endcap(), processor.sector());
+
+        if(!res)
+        {
+            commandStatus = ActionSnapshot::kError;
+        }
+    }
+
+    setProgress(1.);
+
+    return commandStatus;
+}
+
+bool AlignmentFifoDelay::setFixedAlignmentFifoDelays(int endcap, int sector)
+{
+    return true;
+}
 
 } // namespace
 
